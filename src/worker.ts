a199ddfc8a201bd53512ec61aef4d264c3b51468
rohitJung <rokshh28@gmail.com>
2026-09-07 22:28:56 +0545

    chore: remove logs

--- src/worker.ts
+++ src/worker.ts
@@ -10,23 +10,7 @@ interface Env {
 
 export default {
   async fetch(request: Request, env: Env) {
-    console.log("env.DesignAgent:", env.DesignAgent);
-    // inspects request finds durable object and also handles websocket request
-    console.log(
-      "request received",
-      request.method,
-      request.url,
-      request.headers.get("Upgrade"),
-      request.headers.get("Connection"),
-    );
-
     const response = await routeAgentRequest(request, env);
-    console.log("Response", response?.body);
-    console.log(
-      "routeAgentRequest:",
-      response ? `${response.status} ${response.statusText}` : "undefined",
-    );
-
     return response || new Response("Not found", { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
